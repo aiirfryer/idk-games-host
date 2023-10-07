ddc71666578283b222948f63fed2f2ea508b2fe1
aiirfryer <142753911+aiirfryer@users.noreply.github.com>
2023-10-07 16:46:04 -0700

    Update middleware.js
    hey guys...... get waht... chicken but!!1@!11! (please help im being held hostage, my coordinates are 10°22'26"S⁩ ⁦46°34'58"W)

--- middleware.js
+++ middleware.js
@@ -3,6 +3,7 @@ import { next } from '@vercel/edge';
 export default function middleware(req) {
   return next({
     headers: {
+      Content-Security-Policy: frame-ancestors 'self' https://sites.google.com/view/idkg4mes/home;
       'Referrer-Policy': 'origin-when-cross-origin',
       'X-Frame-Options': 'SAMEORIGIN',
       'X-Content-Type-Options': 'nosniff',
